c15d8919246ac8bc1921bd75ab33d17051658ae3
alti-tude <kartik.gupta0204@gmail.com>
2020-04-22 06:45:32 +0530

    initiators for yoyo

--- src/test.cpp
+++ src/test.cpp
@@ -3,38 +3,48 @@
 #include "mpi.h"
 #include "iostream"
 
-long long int Min(long long int a, long long int b){
-    return std::min(a,b);
-}
+const long long int TAGS_INIT = topo::TAGS_CUSTOM_BASE+1;
+
+struct InitMsg{
+    long long int rank;
+    long long int tag;
+    InitMsg(): rank(topo::rank), tag(TAGS_INIT) {}
+};
+
+struct NodeInfo{
+    long long int rank;
+    bool is_awake;
+    NodeInfo(): rank(topo::rank), is_awake(0) {}
+};
 
 void run(){
 
-    // if(topo::rank==0) {
-    //     std::vector<long long int> v = {1,2,3};
-    //     for(int i=0;i<topo::num_neighbours;i++) {
-    //         std::cout << "sent to " << i << "\n";
-    //         topo::send_to_neighbour(v, i, 101);
-    //     }
-    // }
-    // else{
-    //     std::vector<long long int> v = topo::recv_from_neighbour(MPI_ANY_SOURCE, MPI_ANY_TAG, true, true);
-    //     std::cout << topo::rank << ": ";
-    //     for(auto it:v) std::cout << it << " ";
-    //     std::cout << std::endl;
-    // }
-    // MPI_Barrier(MPI_COMM_WORLD);
-    if(topo::is_initiator) std::cout << "initiator" << " " << topo::rank << std::endl;
-    // long long int val=topo::rank+1;
-    // topo::reduce_neighbours(&val, &Min);
+    NodeInfo node;
 
-    // std::cout << topo::rank << " " << val << std::endl
-    ; 
-    // if(topo::rank==0){
-    //     for(auto it:topo::globals){
-    //         for(auto jt:it){
-    //             std::cout << jt << " ";
-    //         }
-    //         std::cout << std::endl;
-    //     }
-    // }
+    if(topo::is_initiator){
+        InitMsg send_init_msg;
+        std::vector<long long int> buffer = topo::marshal(send_init_msg);
+
+        for(int i=0;i<topo::num_neighbours;i++) topo::send_to_neighbour(buffer, i, TAGS_INIT);
+    }
+
+    while(true){
+        std::vector<long long int> recvd_msg = topo::recv_from_neighbour(MPI_ANY_SOURCE, MPI_ANY_TAG, true, true);
+        std::vector<long long int> msg_buffer;
+        for(int i=0;i<recvd_msg.size()-2;i++) msg_buffer.push_back(recvd_msg[i]);
+        long long int source_idx = recvd_msg[recvd_msg.size()-2];
+        long long int tag = recvd_msg[recvd_msg.size()-1];
+
+        if(tag == TAGS_INIT and !node.is_awake){
+            node.is_awake = true;
+            InitMsg send_init_msg;
+            std::vector<long long int> buffer = topo::marshal(send_init_msg);
+
+            for(int i=0;i<topo::num_neighbours;i++) topo::send_to_neighbour(buffer, i, TAGS_INIT);
+        }
+        if(tag == TAGS_INIT){
+            InitMsg recvd_init_msg = topo::unmarshal<InitMsg>(msg_buffer);
+            //process the value recvd from the neighbour.
+        }
+    }
 }
